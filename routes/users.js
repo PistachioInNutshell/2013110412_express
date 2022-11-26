a14dd34bbf2395e9dd96207956b402d4a723fa65
Kittinan <ka.kittinan_st@tni.ac.th>
2022-11-26 11:30:48 +0700

    WorkshopA

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('hello kitty');
+  //res.send('hello kitty');
+  res.status(200).json(
+    {fullname: 'Kittinan Kampusa'})
+});
+
+router.get('/bio', function(req, res, next) {
+  //res.send('hello kitty');
+  res.status(200).json(
+    {fullname : 'Kittinan Kampusa',
+    nickname  : 'Nun',
+    hobby     : 'Gaming',
+    gitusername : 'PistachioInNutshell'})
 });
 
 module.exports = router;
